906aba35615c546714b209b580b9f62314a78063
徐俊 <xujun1019@hotmail.com>
2020-02-03 16:47:18 +0800

    feat: webpack 配置文件 & 新增打包命令

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
