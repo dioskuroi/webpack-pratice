29c072c50b6f30091cb06eea43b63a9f6ca594af
徐俊 <xujun1019@hotmail.com>
2020-02-04 22:00:49 +0800

    feat: 添加mode

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development'
+}
