8dec51e78b8bc3cc62423d1ac5f1ebf3e1246eeb
徐俊 <xujun1019@hotmail.com>
2020-02-07 20:08:02 +0800

    update: mode

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     filename: 'bundle.js',  // ? 输出文件名
     path: path.resolve(__dirname, 'dist') // ? 输出文件路径
   },
-  mode: 'development',  // ? 打包模式
+  mode: 'production',  // ? 打包模式
   module: {
     rules: [{
       test: /\.jpeg$/,  // ? 匹配 jpeg 文件，使用该规则进行打包
