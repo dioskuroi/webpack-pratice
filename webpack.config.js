ac8110fa1e5b50045c458ae77454b44606ef2ad4
徐俊 <xujun1019@hotmail.com>
2020-02-08 20:09:30 +0800

    refact: entry

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const path = require('path')
  * @type {import('webpack').Configuration}
  */
 module.exports = {
-  entry: './src/index.js',  // ? 入口文件配置，也可以是个对象
+  entry: {
+    main: './src/index.js'
+  },  // ? 入口文件配置，也可以是个对象
   output: {                 // ? 打包输出配置
     filename: 'bundle.js',  // ? 输出文件名
     path: path.resolve(__dirname, 'dist') // ? 输出文件路径
