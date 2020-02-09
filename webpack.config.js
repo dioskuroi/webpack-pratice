012809ed1dee97b18d6f9cde344af2edb4598d67
徐俊 <xujun1019@hotmail.com>
2020-02-09 21:58:45 +0800

    feat: 配置 html-webpack-plugin & clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 /**
  * @type {import('webpack').Configuration}
@@ -9,5 +11,11 @@ module.exports = {
     filename: 'bundle.js',  // ? 输出文件名
     path: path.resolve(__dirname, 'dist') // ? 输出文件路径
   },
-  mode: 'development'  // ? 打包模式
+  mode: 'development',  // ? 打包模式
+  plugins: [
+    new HtmlWebpackPlugin({ // ? 会在打包结束后，自动生成一个 html 文件，将打包后的 js 自动引入到 html 中
+      template: './src/index.html'  // ? html 文件模版
+    }),
+    new CleanWebpackPlugin()  // ? 打包前删除 dist 目录下的所有文件
+  ]
 }
