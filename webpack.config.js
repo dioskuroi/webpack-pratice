30e3b1345cb8f088c4c23b84588bcc41453fcaca
徐俊 <xujun1019@hotmail.com>
2020-02-09 20:39:38 +0800

    feat: url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,26 @@ module.exports = {
   },
   mode: 'production',  // ? 打包模式
   module: {
-    rules: [{
-      test: /\.jpeg$/,  // ? 匹配 jpeg 文件，使用该规则进行打包
-      use: {
-        loader: 'file-loader' // ? file-loader 的作用就是将指定文件复制到 dist 目录中，根据规则修改文件名后将文件名返回给 js 中的变量。
+    rules: [
+      // {
+      //   test: /\.jpeg$/,  // ? 匹配 jpeg 文件，使用该规则进行打包
+      //   use: {
+      //     loader: 'file-loader', // ? file-loader 的作用就是将指定文件复制到 dist 目录中，根据规则修改文件名后将文件名返回给 js 中的变量。
+      //     outputPath: 'images/',  // ? 打包路径 dist/images/
+      //     name: '[name]_[hash].[ext]'  // ? 设置打包后的文件名
+      //   }
+      // }
+      {
+        test: /\.(jpeg|jpg|gif|png)$/,
+        use: {
+          loader: 'url-loader', // ? url-loader 可以完全替代 file-loader，还可以将图片打包成base64格式
+          options: {
+            name: '[name]_[hash].[ext]',  // ? 设置打包后的文件名
+            outputPath: 'images/',  // ? 打包路径 dist/images/
+            limit: 20480  // ? 设置 2kb 以下的文件才打包成base64格式
+          }
+        }
       }
-    }]
+    ]
   }
 }
